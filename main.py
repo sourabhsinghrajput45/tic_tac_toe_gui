e0d3296c051956e1e6f0b38bb44f06ec1e1a61f4
Sourabh Rajput <rajputsourabh684@gmail.com>
2025-11-09 22:54:49 +0530

    timer feature added to the game

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from tic-tac-toe-gui!")
+from tic_tac_toe import TicTacToe
+import tkinter as tk
 
+def main():
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
